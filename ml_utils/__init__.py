bbeddda5d3f50cea8e67b103f55178bd7d19b01d
Chinmay Savadikar <savadikarc@gmail.com>
2021-11-10 09:24:43 -0500

    data preprocessing for interval=0

---
+++ ml_utils/__init__.py
@@ -0,0 +1,7 @@
+class DataConstants:
+    SAMPLING_RATE_X = 40 # Hz
+    SAMPLING_RATE_Y = 10 # Hz
+
+# Convenience variables
+X_HEADER = ["acc_x", "acc_y", "acc_z", "gyro_x", "gyro_y", "gyro_z"]
+Y_HEADER = ["label"]
--- ml_utils/dataset.py
+++ ml_utils/dataset.py
@@ -85,3 +85,70 @@ class SubjectDataset(Dataset):
         X = values.reshape(len_array, timesteps, len(X_HEADER))
         
         return np.transpose(X, axes=(0, 2, 1)).copy()
+
+
+class SequentialSubjectDataset(Dataset):
+
+    def __init__(self, datapath: str, ids: list):
+        
+        self.ids = ids
+        self.datapath = datapath
+        self.y_files = {uid: os.path.join(self.datapath, SUBJECT_Y_TEMPLATE.format(parse_uid(uid)[0], parse_uid(uid)[1])) for uid in self.ids}
+        self.x_files = {uid: os.path.join(self.datapath, SUBJECT_X_TEMPLATE.format(parse_uid(uid)[0], parse_uid(uid)[1])) for uid in self.ids}
+        
+        # Generate a list of samples and determine the number of datapoints in the dataset 
+        # and build up the cache
+        self.build_cache_and_datalen()
+
+    def __len__(self):
+        return self.num_samples
+
+    def __getitem__(self, index):
+        
+        inputs = self.X[index]
+        labels = np.array([self.y[index]])
+
+        return torch.from_numpy(inputs), torch.from_numpy(labels)
+
+    def build_cache_and_datalen(self):
+
+        num_samples = 0
+        timesteps = None
+
+        X_list = []
+        y_list = []
+
+        for uid, y_file in self.y_files.items():
+            print(f"Converting uid {uid}")
+            y = pd.read_csv(y_file)
+            n_samples = len(y)
+            num_samples += n_samples
+
+            x_file = self.x_files[uid]
+            X_dataframe = pd.read_csv(x_file)
+            if timesteps is None:
+                _sample = X_dataframe[X_dataframe["timestamp"] == 0]
+                timesteps = len(_sample)
+
+            # Convert to numpy
+            X = self.dataframe_to_numpy(X_dataframe, timesteps, y)
+
+            X_list.append(X)
+            y_list.append(y["label"].values)
+
+        self.X = np.concatenate(X_list, axis=0).astype(np.float32)
+        self.y = np.concatenate(y_list, axis=0).astype(int)
+        assert self.X.shape[0] == self.y.shape[0]
+        
+        self.num_samples = self.X.shape[0]
+
+    def dataframe_to_numpy(self, df, timesteps, y_df):
+        """Convert from pandas to numpy for faster access
+        """
+        len_array = int(len(df) / timesteps)
+        assert len_array == len(y_df)
+
+        values = df[X_HEADER].values
+        X = values.reshape(len_array, timesteps, len(X_HEADER))
+        
+        return np.transpose(X, axes=(0, 2, 1)).copy()
