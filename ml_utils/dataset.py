da1f1f10442b76a301e690dcfbd6340ad9dd5a5b
Chinmay Savadikar <savadikarc@gmail.com>
2021-11-10 22:00:58 -0500

    sequential dataset changes

--- ml_utils/dataset.py
+++ ml_utils/dataset.py
@@ -87,8 +87,10 @@ class SubjectDataset(Dataset):
 
 class SequentialSubjectDataset(Dataset):
 
-    def __init__(self, datapath: str, ids: list, sequence_length: int=40):
+    def __init__(self, datapath: str, ids: list, sequence_length: int=10, phase: str="train"):
         
+        assert phase in ["train", "test"]
+        self.phase = phase
         self.ids = ids
         self.datapath = datapath
         self.y_files = {uid: os.path.join(self.datapath, SUBJECT_Y_TEMPLATE.format(parse_uid(uid)[0], parse_uid(uid)[1])) for uid in self.ids}
@@ -103,11 +105,20 @@ class SequentialSubjectDataset(Dataset):
         return self.num_samples
 
     def __getitem__(self, index):
-        
-        lower = max(0, index - self.sequence_length)
 
-        inputs = self.X[lower:index]
-        labels = self.y[lower:index]
+        if self.phase == "train":
+        
+            lower = max(0, index - self.sequence_length)
+
+            inputs = self.X[lower:index, :]
+            labels = np.expand_dims(self.y[lower:index], axis=0)
+        else:
+            inputs = self.X[[index], :, :]
+            labels = self.y[[index], :]
+            # Remove the dummy appended values
+            valid_indices = labels[0] != -1
+            inputs = inputs[:, valid_indices]
+            labels = labels[:, valid_indices]
 
         return torch.from_numpy(inputs), torch.from_numpy(labels)
 
@@ -132,16 +143,28 @@ class SequentialSubjectDataset(Dataset):
                 timesteps = len(_sample)
 
             # Convert to numpy
-            X = self.dataframe_to_numpy(X_dataframe, timesteps, y)
+            X, y = self.dataframe_to_numpy(X_dataframe, timesteps, y)
 
             X_list.append(X)
-            y_list.append(y["label"].values)
+            y_list.append(y)
 
-        self.X = np.concatenate(X_list, axis=0).astype(np.float32)
-        self.y = np.concatenate(y_list, axis=0).astype(int)
-        assert self.X.shape[0] == self.y.shape[0]
-        
-        self.num_samples = self.X.shape[0]
+        X = np.concatenate(X_list, axis=0).astype(np.float32)
+        y = np.concatenate(y_list, axis=0).astype(np.int64)
+        assert X.shape[0] == y.shape[0]
+
+        self.num_samples, self.feature_size = X.shape
+
+        if self.phase == "test":
+            # Group timesteps together
+            steps = self.num_samples // self.sequence_length
+            X = X.reshape(steps, self.sequence_length, self.feature_size)
+            y = y.reshape(steps, self.sequence_length)
+
+        self.X = X.copy()
+        self.y = y.copy()
+
+        self.num_samples = X.shape[0]
+        assert self.num_samples == self.y.shape[0]
 
     def dataframe_to_numpy(self, df, timesteps, y_df):
         """Convert from pandas to numpy for faster access
@@ -150,6 +173,24 @@ class SequentialSubjectDataset(Dataset):
         assert len_array == len(y_df)
 
         values = df[X_HEADER].values
+        y = y_df["label"].values
+
         X = values.reshape(len_array, timesteps, len(X_HEADER))
+        X = X.reshape(len_array, timesteps*len(X_HEADER))
+
+        num_samples, feature_size = X.shape
+
+        steps = num_samples // self.sequence_length
+        if (num_samples % self.sequence_length) != 0:
+            steps += 1
+            # Append some dummy values to X and y. Remove in __getitem__
+            num_additional = steps*self.sequence_length - num_samples
+            
+            X = np.concatenate([X, np.zeros((num_additional, feature_size))], axis=0)
+            y = np.concatenate([y, -1*np.ones((num_additional,), dtype=np.int64)], axis=0)
         
         return X.copy()
+
+    def collate_batch(self, batch):
+        inputs, outputs = zip(*batch)
+        return torch.stack(inputs), torch.stack(outputs)
